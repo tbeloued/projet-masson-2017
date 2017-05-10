c176ee880fc65594fcfb92db07ea3d1ad7208847
Saad Achemlal <saad.achemlal@gmail.com>
2017-05-10 21:32:20 +0200

    programme 3 TP1

---
+++ TP1/tp1_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+
+  int x = 6500;
+  char y = x;
+  
+
+  printf("la variable x vaut %d \n",x) ;
+  printf("la variable y vaut %c\n",y) ;
+ 
+  return EXIT_SUCCESS;
+}
